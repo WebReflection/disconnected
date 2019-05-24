b6bf6e0b4d5788b1b61589b77f8e3b9e2171a024
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2019-05-24 12:50:28 +0200

    even better/smaller/faster/stronger

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 /*! (c) Andrea Giammarchi */
 function disconnected(poly) {'use strict';
-  var CONNECTED = 'connected';
-  var DISCONNECTED = 'dis' + CONNECTED;
   var Event = poly.Event;
   var WeakSet = poly.WeakSet;
   var notObserving = true;
@@ -16,9 +14,8 @@ function disconnected(poly) {'use strict';
     return node;
   };
   function startObserving(document) {
-    var dispatched = {};
-    dispatched[CONNECTED] = new WeakSet;
-    dispatched[DISCONNECTED] = new WeakSet;
+    var connected = new WeakSet;
+    var disconnected = new WeakSet;
     try {
       (new MutationObserver(changes)).observe(
         document,
@@ -60,24 +57,24 @@ function disconnected(poly) {'use strict';
         i = 0; i < length; i++
       ) {
         record = records[i];
-        dispatchAll(record.removedNodes, DISCONNECTED, CONNECTED);
-        dispatchAll(record.addedNodes, CONNECTED, DISCONNECTED);
+        dispatchAll(record.removedNodes, 'disconnected', disconnected, connected);
+        dispatchAll(record.addedNodes, 'connected', connected, disconnected);
       }
     }
-    function dispatchAll(nodes, type, counter) {
+    function dispatchAll(nodes, type, wsin, wsout) {
       for (var
         node,
         event = new Event(type),
         length = nodes.length,
         i = 0; i < length;
         (node = nodes[i++]).nodeType === 1 &&
-        dispatchTarget(node, event, type, counter)
+        dispatchTarget(node, event, type, wsin, wsout)
       );
     }
-    function dispatchTarget(node, event, type, counter) {
-      if (observer.has(node) && !dispatched[type].has(node)) {
-        dispatched[counter].delete(node);
-        dispatched[type].add(node);
+    function dispatchTarget(node, event, type, wsin, wsout) {
+      if (observer.has(node) && !wsin.has(node)) {
+        wsout.delete(node);
+        wsin.add(node);
         node.dispatchEvent(event);
         /*
         // The event is not bubbling (perf reason: should it?),
@@ -99,7 +96,7 @@ function disconnected(poly) {'use strict';
         children = node.children || [],
         length = children.length,
         i = 0; i < length;
-        dispatchTarget(children[i++], event, type, counter)
+        dispatchTarget(children[i++], event, type, wsin, wsout)
       );
     }
   }
